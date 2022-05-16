6ddf20a6016836d2ed0d5b8aed9e9abd452137d1
Alif_Rahman <alifjcc99@gmail.com>
2022-05-16 11:12:20 +0600

    Add files via upload

---
+++ start/start.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello world"<<endl;
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+
+    return 0;
+}
